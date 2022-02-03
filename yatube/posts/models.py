a543344f8d910e45d9eddd2bbb19a9a21d6eda72
Cap Pavel <cap@Caps-MacBook-Air.local>
2022-02-03 22:35:55 +0300

    add help_text to model and urls tests

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,16 +5,28 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Название")
-    slug = models.SlugField(unique=True)
-    description = models.TextField(verbose_name="Описание")
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Название",
+        help_text="Введите название группы",
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name="Ссылка на группу",
+        help_text="Укажите ссылку на группу",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание группы"
+    )
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField(verbose_name="Текст записи")
+    text = models.TextField(
+        verbose_name="Текст записи", help_text="Введите текст поста"
+    )
     pub_date = models.DateTimeField(
         auto_now_add=True, verbose_name="Дата публикации"
     )
@@ -31,10 +43,11 @@ class Post(models.Model):
         null=True,
         related_name="posts",
         verbose_name="Группа",
+        help_text="Выберите группу",
     )
 
     class Meta:
         ordering = ["-pub_date"]
 
     def __str__(self) -> str:
-        return self.text
+        return self.text[:15]
